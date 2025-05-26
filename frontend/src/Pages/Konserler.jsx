2acc3b9d29a0e9f382c03bf63e398f5dc8676318
Mehmet Can ÇALIŞKAN <caliskanmehmetc@gmail.com>
2025-05-26 13:35:06 +0300

    foto eklendi

--- frontend/src/Pages/Konserler.jsx
+++ frontend/src/Pages/Konserler.jsx
@@ -1,11 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-const konserler = [
+ 
+
+const konserler_rock = [
+  { id: 1, ad: "Hayko Cepkin", img:"/images/hayko.jpg" },
+  { id: 2, ad: "Queen", img: "/images/queen.jpg" },
+  { id: 3, ad: "Pink Floyd", img: "https://placehold.co/300x200?text=Konser+Z" },
+  { id: 4, ad: "Mor ve Ötesi", img: "https://placehold.co/300x200?text=Konser+W" },
+];
+const konserler_caz = [
   { id: 1, ad: "Konser X", img: "https://placehold.co/300x200?text=Konser+X" },
   { id: 2, ad: "Konser Y", img: "https://placehold.co/300x200?text=Konser+Y" },
   { id: 3, ad: "Konser Z", img: "https://placehold.co/300x200?text=Konser+Z" },
   { id: 4, ad: "Konser W", img: "https://placehold.co/300x200?text=Konser+W" },
 ];
+
 
 const EtkinlikKart = ({ etkinlik }) => (
   <div className="col-6 col-md-4 col-lg-3 d-flex flex-column align-items-center mb-4">
@@ -28,13 +37,20 @@ const EtkinlikKart = ({ etkinlik }) => (
 const Konserler = () => {
   return (
     <div className="container my-4">
-      <h1 className="mb-4">Tüm Konserler</h1>
+      <h1 className="mb-4">Rock Konserleri</h1>
       <div className="row">
-        {konserler.map(konser => (
+        {konserler_rock.map(konser => (
+          <EtkinlikKart key={konser.id} etkinlik={konser} />
+        ))}
+      </div>
+      <h1>Caz Konserleri</h1>
+      <div className="row">
+       {konserler_caz.map(konser => (
           <EtkinlikKart key={konser.id} etkinlik={konser} />
         ))}
       </div>
     </div>
+
   );
 };
 
