be1616b7287920e643895e2165c5ee843dac7ef7
İsmail Çolak <ismail.colak@ogr.iuc.edu.tr>
2025-06-01 11:03:33 +0300

    topbar user

--- src/Components/Topbar.jsx
+++ src/Components/Topbar.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../data/Context';
 
 const Topbar = () =>
 {
+    const { user, setUser } = useContext(UserContext);
+
     return (
         <nav
             className="navbar navbar-expand-lg navbar-dark shadow-sm"
@@ -80,14 +84,20 @@ const Topbar = () =>
 
                     {/* Sağ Butonlar */}
                     <div className="d-flex align-items-center">
-                        <Link to="/login" className="btn btn-outline-light me-2" style={{ fontSize: "14px" }}>Giriş Yap</Link>
-                        <Link to="/profile" className="btn btn-outline-light d-flex align-items-center" style={{ fontSize: "14px" }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-person-circle me-1" viewBox="0 0 16 16">
-                                <path d="M13.468 12.37C12.758 11.226 11.482 10.5 10 10.5c-1.482 0-2.758.726-3.468 1.87A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z" />
-                                <path fillRule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0 8A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                            </svg>
-                            Profil
-                        </Link>
+                        {user ? (
+                            <>
+                                <Link to="/profile" className="btn btn-outline-light me-2" style={{ fontSize: "14px" }}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-person-circle me-1" viewBox="0 0 16 16">
+                                        <path d="M13.468 12.37C12.758 11.226 11.482 10.5 10 10.5c-1.482 0-2.758.726-3.468 1.87A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z" />
+                                        <path fillRule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0 8A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                    </svg>
+                                    {user.name || user.username || user.email}
+                                </Link>
+                                <button className="btn btn-outline-light btn-danger me-2" style={{ fontSize: "14px" }} onClick={() => setUser(null)}>Çıkış</button>
+                            </>
+                        ) : (
+                            <Link to="/login" className="btn btn-outline-light me-2" style={{ fontSize: "14px" }}>Giriş Yap</Link>
+                        )}
                     </div>
                 </div>
             </div>
